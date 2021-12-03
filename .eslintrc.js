ed014bb0f44ce238d1dcacc77a650d3da1ecb30d
蔡金锋 <jeff.cai@99bill.com>
2021-12-03 15:29:48 +0800

    chore: 添加eslint-plugin-react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,13 +17,15 @@ module.exports = {
     ecmaVersion: 13,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   settings: {
     react: {
       version: 'detect',
     },
   },
   rules: {
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
     'react/display-name': 0,
     '@typescript-eslint/ban-ts-comment': 0,
   },
